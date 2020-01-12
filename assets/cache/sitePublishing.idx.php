12562490ea514f52b93fd654463a285261edb9c6
Maxrozvora <rozvora.max@gmail.com>
2020-01-12 11:33:59 +0200

    first commit

---
+++ assets/cache/sitePublishing.idx.php
@@ -0,0 +1,4 @@
+<?php
+$recent_update='1578820840';
+$cacheRefreshTime='0';
+
---
+++ manager/includes/version.inc.php
@@ -0,0 +1,5 @@
+<?php
+$modx_version      = '1.4.9'; // Current version number
+$modx_release_date = 'May 26, 2019'; // Date of release
+$modx_branch       = 'Evolution'; // Codebase name
+$modx_full_appname = "{$modx_branch} {$modx_version} ({$modx_release_date})";
